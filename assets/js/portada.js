a8556eea2851c9ca696661782b1bff11a44afb31
Ana Durand <ana.dc1004@gmail.com>
2017-04-25 14:18:51 -0500

    creando un new header y funcion unload

--- assets/js/portada.js
+++ assets/js/portada.js
@@ -1,67 +1,30 @@
 
 window.addEventListener("load", function(){
-  var body = document.getElementById("body");
-  var firstImg = document.getElementById("firstImg");
-  var secondImg = document.getElementById("secondImg");
-  var thirdImg = document.getElementById("thirdImg");
-  var leftButton = document.getElementById("leftButton");
-  var rightButton = document.getElementById("rightButton");
-  var portadaContent = document.getElementsByClassName("portada-contenido")[0];
-  //addEventListener para mover el carrusel con las flechas del teclado
-  body.addEventListener("keyup", keyboardMove);
-  //addEventListener a los botones de desplazamiento con click
-  leftButton.addEventListener("click", function(e){
-    moveImag(-1);
-    portadaContent.classList.remove("change-slide");
-  });
-  rightButton.addEventListener("click", function(e){
-    moveImag(1);
-    portadaContent.classList.add("change-slide");
-  });
-
+  slideShow();
 
 });
-
-//durante un intervalo de tiempo se ejecuta la funcion callback dash
-var imgIntervalo = setInterval(dash,3000);
-//funcion que mueve las imagenes
-function dash(){
-  moveImag(1);
-}
-//funcion mueve las imagenes con las flechas del teclado
-function keyboardMove(e){
-  if(e.keyCode == 37){ //flecha de la izquierda
-    moveImag(-1);
-    remove();
-  }
-  if(e.keyCode == 39){
-    moveImag(1);
-    add();
+window.onbeforeunload = preguntarSalir;
+var pregunta = true;
+function preguntarSalir(){
+  if(pregunta){
+     var respuesta = confirm();
+    if(respuesta){
+      window.onunload = function(){return true;}
+    }else{
+      return false;
+    }
   }
 }
-// funciones del carrusel de imagenes
- var actualImg = 1;
- var ImgUrl = [["nails1.jpg","nails2.jpg","nails3.jpg"],["nails4.jpg","nails5.jpg","nails6.jpg"],["nails7.jpg","nails8.jpg","nails9.jpg"],["nails10.jpg","nails11.jpg","nails12.jpg"]];
-
- function moveImag(n){
-   actualImg += n;
-   if(actualImg > ImgUrl.length){
-     actualImg = 1;
-   }
-   if(actualImg  < 1){
-     actualImg = ImgUrl.length;
-   }
-   firstImg.src = "assets/img/portada/" + ImgUrl[actualImg -1][0];
-   secondImg.src = "assets/img/portada/" + ImgUrl[actualImg-1][1];
-   thirdImg.src = "assets/img/portada/" + ImgUrl[actualImg -1][2];
-
- }
- function remove(){
-   var portadaContent = document.getElementsByClassName("portada-contenido")[0];
-   portadaContent.classList.remove("change-slide");
- }
- function add(){
-   var portadaContent = document.getElementsByClassName("portada-contenido")[0];
-   portadaContent.classList.add("change-slide");
-
- }
+// Funcion que cambia las imagenes de portada
+var imgActual = 0;
+function slideShow(){
+  var img = document.getElementsByClassName("portada-img");
+  for(i=0; i<img.length;i++){
+    img[i].classList.add("display-none");
+  }
+  imgActual++;
+  if(imgActual>img.length){ imgActual=1;}
+  img[imgActual-1].classList.remove("display-none");
+  //durante un intervalo de tiempo se ejecuta la funcion callback slideShow
+  setTimeout(slideShow, 2000);
+}
